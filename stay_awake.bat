ba66289aa25e079887890fc862015afe471bd418
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2024-08-02 23:44:27 -0400

    Update stay_awake.bat
    version 0.6

--- stay_awake.bat
+++ stay_awake.bat
@@ -2,6 +2,6 @@
 :a
   tree
 goto a
-REM experimental Windows-only script to keep a system from ever going to sleep.
+REM experimental Windows-only script to keep a system from ever going to sleep by running code in an infinite loop.
 REM developed by K0K0$HA to find systems in a large building.
-REM Beta phase, no code written
+REM v. 0.6
